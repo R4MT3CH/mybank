a810cc100a45043afc035860efb5da0ca3c19343
Ramin Roufeh <raminroufeh@gmail.com>
2024-05-02 20:08:44 -0700

    Implemnt the comple layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 function Home() {
+  const loggedIn = { firstName: "Ramin", lastName:"Roufeh",email:"contact@gmail.com" };
+
   return (
-    <div>Home</div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1234.5}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 1234.50},{currentBalance: 500.50}]}
+      />
+    </section>
+  );
 }
 
-export default Home
+export default Home;
